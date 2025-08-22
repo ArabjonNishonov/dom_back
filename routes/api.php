3702ff67266d2a4d8a8e2663bb559a4cda26c8ef
ArabkhonDev <starc9899@gmail.com>
2025-08-22 17:33:05 +0500

    api yangilandi

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/favorite', function (Request $request) {
+    $data = [
+        'title' => 'Favorite Products',
+        'body' => 'Favorite Products',
+        'price' => 400,
+        'quantity' => 3,
+    ];
+    return response()->json(['message' => 'salom', 'data' => $data]);
+});
