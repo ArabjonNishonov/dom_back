50b8378792bcd269d194a99c9ceebc88e80f9a39
ArabkhonDev <starc9899@gmail.com>
2025-08-30 00:59:09 +0500

    welcome header has been done

--- database/migrations/2024_08_21_051150_create_roles_table.php
+++ database/migrations/2024_08_21_051150_create_roles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+        Role::create(['name' => env('role')]);
     }
 
     /**
